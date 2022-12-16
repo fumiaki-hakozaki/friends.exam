0b9496ba62738001b8c40d5635346c11faed3e85
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2022-12-17 01:17:46 +0900

    2つめのモデル作成、CRUD機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.derete(:user_id)
+    session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,12 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        if @user.save
-            redirect_to user_path(@user.id)
-        else
-            render :new
-        end
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
-
+            redirect_to user_path(@user.id)
         else
             render :new
         end
